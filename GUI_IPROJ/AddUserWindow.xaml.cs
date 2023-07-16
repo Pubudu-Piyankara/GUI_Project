4adfbbd6845fca1232068aca592b6a6bf528436f
Piyankara N W M G P <pubudupiyankara.me@gmail.com>
2023-07-16 21:43:26 +0530

    GUI_Project

---
+++ GUI_IPROJ/AddUserWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI_IPROJ
+{
+    /// <summary>
+    /// Interaction logic for AddUserWindow.xaml
+    /// </summary>
+    public partial class AddUserWindow : Window
+    {
+        public AddUserWindow(AddUserVM vm)
+
+        {
+            InitializeComponent();
+            DataContext = vm;
+            vm.CloseAction = () => Close();
+        }
+        private void Button_Click_Back(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+
+            mainWindow.Show();
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+           
+            CheckBox clickedCheckBox = (CheckBox)sender;
+
+            
+            foreach (CheckBox checkBox in new CheckBox[] { Option1CheckBox, Option2CheckBox })
+            {
+                if (checkBox != clickedCheckBox)
+                {
+                    checkBox.IsChecked = false;
+                }
+            }
+
+            
+            if (Option1CheckBox.IsChecked == true)
+            {
+                
+            }
+            else if (Option2CheckBox.IsChecked == true)
+            {
+                
+            }
+        }
+    }
+}
---
+++ GUI_IPROJ/MainWindowVM.cs
@@ -0,0 +1,104 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GUI_IPROJ.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Windows;
+
+namespace GUI_IPROJ
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        public ObservableCollection<User> users;
+        [ObservableProperty]
+        public User selectedUser = null;
+
+        public void CloseMainWindow()
+        {
+            Application.Current.MainWindow.Close();
+        }
+
+        [RelayCommand]
+        public void message()
+        {
+
+            MessageBox.Show($"{selectedUser.FirstName} GPA value must be between 0 and 4!", "Error!");
+        }
+
+        [RelayCommand]
+        public void AddNewStudent()
+        {
+            var vm = new AddUserVM();
+            vm.title = "ADD USER";
+            AddUserWindow window = new AddUserWindow(vm);
+            window.ShowDialog();
+
+            if (vm.Student.FirstName != null)
+            {
+                users.Add(vm.Student);
+            }
+        }
+
+        [RelayCommand]
+        public void Delete()
+        {
+            if (selectedUser != null)
+            {
+                string name = selectedUser.FirstName;
+                users.Remove(selectedUser);
+                MessageBox.Show($"{name} is Deleted Successfuly!", "DELETED! \a ");
+            }
+            else
+            {
+                MessageBox.Show("Please Select User!", "Error!");
+            }
+        }
+        [RelayCommand]
+        public void EditStudent()
+        {
+            if (selectedUser != null)
+            {
+                var vm = new AddUserVM(selectedUser);
+                vm.title = "EDIT USER";
+                var window = new AddUserWindow(vm);
+
+                window.ShowDialog();
+
+                int index = users.IndexOf(selectedUser);
+                users.RemoveAt(index);
+                users.Insert(index, vm.Student);
+
+            }
+
+            else
+            {
+                MessageBox.Show("Please Select the User!", "Error!");
+            }
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MainWindowVM()
+        {
+            users = new ObservableCollection<User>();
+                BitmapImage image1 = new BitmapImage(new Uri("\\Model\\Images\\1.png", UriKind.Relative));
+                users.Add(new User(23, "Nishan", "Madushanka", "12/12/1999", 3.2, image1));
+            BitmapImage image2 = new BitmapImage(new Uri("\\Model\\Images\\2.png", UriKind.Relative));
+            users.Add(new User(24, "Chathuranga", "Hewavitharana", "12/12/1999", 3.0, image2));
+            BitmapImage image3 = new BitmapImage(new Uri("\\Model\\Images\\3.png", UriKind.Relative));
+            users.Add(new User(24, "Thambara ", "Dahanayake", "12/12/1999", 3.1, image3));
+            BitmapImage image4 = new BitmapImage(new Uri("\\Model\\Images\\4.png", UriKind.Relative));
+            users.Add(new User(21, "Lakshan", "Miyuranga", "12/12/1999", 3.8, image4));
+
+        }
+    }
+}
